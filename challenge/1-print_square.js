81a55932ac3b487a18b5aabb85b9a00e0b5b8137
SaLaam17 <slaamri@yahoo.fr>
2025-05-12 11:26:17 +0200

    1-print_square.js

---
+++ challenge/1-print_square.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+/*
+    Print a square with the character #
+    
+    The size of the square must be the first argument 
+    of the program.
+*/
+
+if (process.argv.length <= 2) {
+    process.stderr.write("Missing argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1)
+}
+/*
+  In the line: parseInt(process.argv[2], 16), 16 means hexadecimal.
+  Which is the wrong base to use.
+  We have to use base 10 instead, which means decimal.
+  size has no type declaration in the code to be fixed
+*/
+let size = parseInt(process.argv[2], 10)
+
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
+        process.stdout.write("#");
+    }
+    process.stdout.write("\n");
+}
